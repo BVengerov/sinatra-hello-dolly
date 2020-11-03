61d8cc97ca351997e5c4c7b39c7b800d790573db
Borys Vengerov <borys.vengerov@gmail.com>
2020-11-03 16:01:41 +0100

    Added templates and more routes

--- hello.rb
+++ hello.rb
@@ -3,3 +3,15 @@ require "sinatra"
 get "/hello" do
   "<h1>Hello, Dolly!</h1>"
 end
+
+get "/bonjour" do
+  "<h1>Bonjour, mademoiselle Dolly!</h1>"
+end
+
+get "/servus" do
+  "<h1>Servus, Frau Dolly!</h1>"
+end
+
+get "/" do
+  erb :welcome
+end
