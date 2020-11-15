eec89daac87c8cdbacf77a31570e298e6cfb3df3
Borys Vengerov <borys.vengerov@gmail.com>
2020-11-15 13:14:16 +0100

    Reading files

--- hello.rb
+++ hello.rb
@@ -2,16 +2,25 @@
 
 require 'sinatra'
 
+NAME = 'Dolly'
+
+def get_greeting(language, name)
+  greeting = File.read("greetings/#{language}.txt")
+  "#{greeting} #{name}!"
+rescue Errno::ENOENT
+  nil
+end
+
 get '/hello' do
-  '<h1>Hello, Dolly!</h1>'
+  "<h1>#{get_greeting('en_US', NAME)}</h1>"
 end
 
 get '/bonjour' do
-  '<h1>Bonjour, mademoiselle Dolly!</h1>'
+  "<h1>#{get_greeting('fr_FR', NAME)}</h1>"
 end
 
 get '/servus' do
-  '<h1>Servus, Frau Dolly!</h1>'
+  "<h1>#{get_greeting('de_BY', NAME)}</h1>"
 end
 
 get '/' do
